6d2b2ea21d17539b460cf044dde2b16d99aae783
ecaps24 <edwin.f.capidos@gmail.com>
2022-01-26 12:39:08 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,70 @@
+function checkUndefinedModifier(blockElement, modifier) {
+  if (modifier === 'undefined') {
+
+      console.warn(
+      
+      );
+  }
+}
+
+function computeModifiers(blockElement, modifiers) {
+  return modifiers
+      .map(m => {
+          if (typeof m === 'object') {
+              let result = [];
+              for (let k in m) {
+                  if (m.hasOwnProperty(k)) {
+                      if (process.env.DEBUG) {
+                          checkUndefinedModifier(blockElement, k);
+                      }
+                      const v = m[k];
+                      if (v) {
+                          result.push(`${blockElement}--${k}`);
+                      }
+                  }
+              }
+              return result.join(' ');
+          }
+          if (process.env.DEBUG) {
+              checkUndefinedModifier(blockElement, m);
+          }
+          return `${blockElement}--${m}`;
+      })
+      .join(' ');
+}
+
+export function bem(blockElement, ...modifiers) {
+  const computedModifiers = computeModifiers(blockElement, modifiers);
+  if (!computedModifiers) {
+      return blockElement;
+  }
+  return `${blockElement} ${computedModifiers}`;
+}
+
+const bemFactory = {
+  block(blockName) {
+      let factory = (elementName, ...modifiers) => {
+          const blockElement = elementName ? `${blockName}__${elementName}` : blockName;
+          return bem(blockElement, ...modifiers);
+      };
+      factory.themed = theme => {
+          return bemFactory.block(`${blockName}--t-${theme}`);
+      };
+      factory.add = (...classNames) => {
+          const className = classNames.join(' ');
+          if (!className) {
+              return factory;
+          }
+          return (...args) => {
+              return `${classNames} ${factory(...args)}`;
+          };
+      };
+      return factory;
+  },
+
+  qa(blockName) {
+      return bemFactory.block(blockName);
+  }
+};
+
+export default bemFactory;
